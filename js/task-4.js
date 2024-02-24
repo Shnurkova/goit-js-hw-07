08b1d5f3af5b62e3d33a41ac6c47a0db1a15429f
Shnurkova <Annshnur@gmail.com>
2024-02-24 19:19:52 +0200

    fix task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,43 +1,21 @@
-// const form = document.querySelector(".login-form");
+const form = document.querySelector(".login-form");
 
-// form.addEventListener("submit", handleSubmit)
+    form.addEventListener("submit", handleSubmit)
 
-// function handleSubmit(event) {
-//     event.preventDefault();
-
-//     const isValid = {};
-
-
-//     const elements = event.currentTarget.elements;
-
-//     if (elements.email.value || elements.password.value === null) {
-//         alert(`All form fields must be filled in`);
-//     }
-
-//     const info = {
-//         email: elements.email.value.trim(),
-//         password: elements.password.value.trim(),
-//     }
-//     console.log(info);
-
-//     event.currentTarget.reset();
-// }
-
-const loginForm = document.querySelector('.login-form')
-
-loginForm.addEventListener('submit', event => {
+    function handleSubmit(event) {
+        
     event.preventDefault();
-    const isValid = {};
-    for (const input of loginForm.elements) {
-        if (input.type === 'email' || input.type === 'password') {
-            isValid[input.name] = input.value.trim();
-        }
-}
-    if (Object.values(isValid).some(value => value === '')) {
-        alert('All form fields must be filled in');
+
+    const elements = event.currentTarget.elements;
+    if (form.email.value.trim() === "" || form.password.value.trim() === "") {
+        alert(`All form fields must be filled in`);
     }
-    else {
-        console.log(isValid);
-        loginForm.reset();
+
+    const info = {
+        email: elements.email.value,
+        password: elements.password.value,
     }
-})
+    console.log(info);
+
+    event.currentTarget.reset();
+}  
